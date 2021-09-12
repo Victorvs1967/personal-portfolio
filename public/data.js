67b04fff6205771aa5edcd6cb6fcbada675b0ba3
Victors1967 <victorsmirnov@me.com>
2021-09-12 20:22:21 +0300

    add js

--- public/data.js
+++ public/data.js
@@ -2,12 +2,19 @@ const projects = [
     {
         name: "project one",
         tags: "#javascript, #fullstack, #ui/ux, #backend",
-        image: "project (1).png",
+        image: "project1.png",
     },
     {
         name: "project two",
         tags: "#javascript, #fullstack",
-        image: "project (2).png",
+        image: "project2.png",
+    },
+    {
+        name: "project three",
+        tags: "#java, #mongodb, #angular, #fullstack",
+        image: "project3.png",
     },
     // +8 more
 ]
+
+console.log(project);
