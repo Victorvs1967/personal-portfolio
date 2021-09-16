9ee6da0333bd011e1dbeb93e297e8a9a2e98e11b
Victors1967 <victorsmirnov@me.com>
2021-09-16 14:24:13 +0300

    3333

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ COPY package.json package-lock.json ./
 RUN npm install
 
 ENV PATH="./node_modules/.bin:$PATH"
-ENV EMAIL="real_mail"
+ENV EMAIL="real_email"
 ENV PASSWORD="real_password"
 
 COPY . ./
--- public/js/app.js
+++ public/js/app.js
@@ -1,79 +1,82 @@
-const links = document.querySelectorAll('.link'),
-      projectContainer = document.querySelector('.project-container'),
+const toggleBtn = document.querySelector('.toggle-btn'),
+      linksContainer = document.querySelector('.links-container'),
+      links = document.querySelectorAll('.link'),
       filters = document.querySelectorAll('.filter-btn'),
-      contactBtn = document.querySelector('.contact-btn'),
+      projectContainer = document.querySelector('.project-container'),
       firstName = document.querySelector('.first-name'),
       lastName = document.querySelector('.last-name'),
       email = document.querySelector('.email'),
       msg = document.querySelector('.message'),
-      toggleBtn = document.querySelector('.toggle-btn'),
-      linkContainer = document.querySelector('.links-container');
-
+      contactBtn = document.querySelector('.contact-btn');
 
 const init = () => {
 
-    links.forEach(link => {
-      link.addEventListener('click', () => {
-        links.forEach(ele => ele.classList.remove('active'));
-        link.classList.add('active');
-      });
+  // links decorating
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      links.forEach(ele => ele.classList.remove('active'));
+      link.classList.add('active');
     });
+  });
 
-    projects.forEach(project => {
-      projectContainer.innerHTML += `
-        <div class="project-card" data-tags="#all, ${project.tags}">
-          <img src="img/${project.image}" alt="image">
-          <div class="content">
-            <h1 class="project-name">${project.name}</h1>
-            <span class="tags">${project.tags}</span>
-          </div>
-        </div> 
-      `;
-    });
+  // project cards
+  projects.forEach(project => {
+    projectContainer.innerHTML += `
+      <div class="project-card" data-tags="#all, ${project.tags}">
+        <img src="img/${project.image}" alt="image">
+        <div class="content">
+          <h1 class="project-name">${project.name}</h1>
+          <span class="tags">${project.tags}</span>
+        </div>
+      </div> 
+    `;
+  });
 
-    filters.forEach(filterBtn => {
-      filterBtn.addEventListener('click', () => {
-        const id = filterBtn.getAttribute('id');
-        const projectCards = document.querySelectorAll('.project-card');
-        projectCards.forEach(card => {
-          if (card.getAttribute('data-tags').includes(id)) {
-            card.classList.remove('hide');
-          } else {
-            card.classList.add('hide');
-          }
-        });
-        filters.forEach(btn => btn.classList.remove('active'));
-        filterBtn.classList.add('active');
-      });
-    });
-
-      contactBtn.addEventListener('click', () => {
-        if (firstName.value.length && lastName.value.length && email.value.length && msg.value.length) {
-          fetch('/mail', {
-            method: 'post',
-            headers: new Headers({'Content-Type': 'application/json'}),
-            body: JSON.stringify({
-              firstname: firstName.value,
-              lastname: lastName.value,
-              email: email.value,
-              msg: msg.value,
-            })
-          })
-          .then(res => res.json())
-          .then(data => {
-            alert(data);
-            location.reload();
-          });
-
+  // filter button
+  filters.forEach(filterBtn => {
+    filterBtn.addEventListener('click', () => {
+      const id = filterBtn.getAttribute('id');
+      const projectCards = document.querySelectorAll('.project-card');
+      projectCards.forEach(card => {
+        if (card.getAttribute('data-tags').includes(id)) {
+          card.classList.remove('hide');
+        } else {
+          card.classList.add('hide');
         }
       });
-
-    // toggle button
-    toggleBtn.addEventListener('click', () => {
-      toggleBtn.classList.toggle('active');
-      linkContainer.classList.toggle('show');
+      filters.forEach(btn => btn.classList.remove('active'));
+      filterBtn.classList.add('active');
     });
+  });
 
-  };
+  // contact button
+  contactBtn.addEventListener('click', () => {
+    if (firstName.value.length && lastName.value.length && email.value.length && msg.value.length) {
+      fetch('/mail', {
+        method: 'post',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify({
+          firstname: firstName.value,
+          lastname: lastName.value,
+          email: email.value,
+          msg: msg.value,
+        })
+      })
+      .then(res => res.json())
+      .then(data => {
+        alert(data);
+        location.reload();
+      });
+
+    }
+  });
+
+  // toggle button
+  toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    linksContainer.classList.toggle('show');
+  });
+
+};
 
 init();
